954ee6684f8407038092fb97071964e3cae2cc82
Gerhard Molin <gerhard@minimumbadass.com>
2022-03-22 15:18:53 +0200

    adding redis chache manager

--- src/bookmark/bookmark.module.ts
+++ src/bookmark/bookmark.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { BookmarkController } from './bookmark.controller';
 import { BookmarkService } from './bookmark.service';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
-  controllers: [BookmarkController],
-  providers: [BookmarkService]
+	imports: [CacheModule.register({
+		store: redisStore,
+		host: 'localhost',
+		port: 5003,
+	})],
+	controllers: [BookmarkController],
+	providers: [BookmarkService]
 })
 export class BookmarkModule {}
