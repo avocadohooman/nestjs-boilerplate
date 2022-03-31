c3258b6e129f58b884bf1bc6e4e5f9dc63462352
avocadohooman <gmolin@student.hive.fi>
2022-03-31 15:23:44 +0200

    implemented ApiProperties in order to display dtos in open api

--- src/bookmark/dto/create-bookmark.dto.ts
+++ src/bookmark/dto/create-bookmark.dto.ts
@@ -1,14 +1,18 @@
 import { IsNotEmpty, IsOptional, IsString, IsUrl } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateBookmarkDto {
+	@ApiProperty()
 	@IsString()
 	@IsNotEmpty()
 	title: string;
 
+	@ApiProperty()
 	@IsString()
 	@IsOptional()
 	description?: string;
 
+	@ApiProperty()
 	@IsString()
 	@IsNotEmpty()
 	@IsUrl()
--- src/bookmark/dto/edit-bookmark.dto.ts
+++ src/bookmark/dto/edit-bookmark.dto.ts
@@ -1,14 +1,18 @@
 import { IsOptional, IsString, IsUrl } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
 
 export class EditBookmarkDto {
+	@ApiProperty()
 	@IsString()
 	@IsOptional()
 	title?: string;
 
+	@ApiProperty()
 	@IsString()
 	@IsOptional()
 	description?: string;
 
+	@ApiProperty()
 	@IsString()
 	@IsOptional()
 	@IsUrl()
